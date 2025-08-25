271a8454cf562b0e62a2477315c2508a3f52754c
Abdullah-Jawahir <mjabdullah33@gmail.com>
2025-08-26 00:37:10 +0530

    Update copyright year in footer to use current date dynamically

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -48,7 +48,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 MemoSpark. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} MemoSpark. All rights reserved.</p>
         </div>
       </div>
     </footer>
